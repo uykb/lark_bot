ee239b8d2f1a08ce2f7de51bcedc46dd01ebe9e4
uykb <tuzhenqiang@gmail.com>
2025-03-21 15:54:54 +0800

    添加迟到人员统计

---
+++ src/config/userConfig.js
@@ -0,0 +1,13 @@
+// 考勤统计用户ID配置
+const USER_IDS = [
+  "6f89cd8e", "1d883db7", "7b6f547g", "eg74b574", "b87fg83d", 
+  "43a5cade", "72a34568", "142ffbfb", "473ef9d9", "9ff6e6g9", "b99c86ge", "b7fda3fc", "5d5accf7", "92d8d942", 
+  "5c546878", "549dea55", "a875defc", "35ec3fca", "ceo", 
+  "5618c5ea", "a2gfcd83", "3b9884ae", "fdf39ge6", "43eafg37", 
+  "4e5dd38b", "31f69e2c", "b949eb58", "798489da", "2dff31cd", 
+  "9c1b56ag", "2d6ddg17", "b8cg86bc", "f6e1287e"
+];
+
+module.exports = {
+  USER_IDS
+};
--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -3,16 +3,8 @@ const moment = require('moment');
 const { getAccessToken } = require('./authService');
 const { logger } = require('../utils/logger');
 
-// 硬编码的用户ID列表
-const FIXED_USER_IDS = [
-  "6f89cd8e", "1d883db7", "7b6f547g", "eg74b574", "b87fg83d", 
-  "43a5cade", "72a34568", "142ffbfb", "473ef9d9", "92af688b", 
-  "9ff6e6g9", "b99c86ge", "b7fda3fc", "5d5accf7", "92d8d942", 
-  "5c546878", "549dea55", "a875defc", "35ec3fca", "ceo", 
-  "5618c5ea", "a2gfcd83", "3b9884ae", "fdf39ge6", "43eafg37", 
-  "4e5dd38b", "31f69e2c", "b949eb58", "798489da", "2dff31cd", 
-  "9c1b56ag", "2d6ddg17", "b8cg86bc", "f6e1287e"
-];
+// 从配置文件中导入用户ID列表
+const { USER_IDS } = require('../config/userConfig');
 
 // 获取考勤统计数据
 async function getAttendanceStats() {
